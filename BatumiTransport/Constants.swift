30fd5267cd5d5905deeee2c8c45d0491e73a93b6
Igor Leonovich <mail@domain.com>
2023-02-02 14:53:16 +0400

    Live Routes

---
+++ BatumiTransport/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  BatumiTransport
+//
+//  Created by Igor Leonovich on 2.02.23.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let baseURL = "http://transport.geogps.ge"
+}
--- BatumiTransport/Screens/MapViewController.swift
+++ BatumiTransport/Screens/MapViewController.swift
@@ -9,12 +9,20 @@ import GoogleMaps
 import UIKit
 
 final class MapViewController: UIViewController {
+    
+    private var routeDataTask: URLSessionTask?
+    private lazy var sessionConfiguration: URLSessionConfiguration = {
+        let config = URLSessionConfiguration.default
+        config.httpAdditionalHeaders = ["Content-Type": "application/x-www-form-urlencoded"]
+        return config
+    }()
+    private var liveRoutes = [String: GPS]()
 
     private var mapView: GMSMapView!
     private var initialCoordinates: CLLocationCoordinate2D? {
         didSet {
             guard let initialCoordinates = initialCoordinates else { return }
-            let camera = GMSCameraPosition.camera(withLatitude: initialCoordinates.latitude, longitude: initialCoordinates.longitude, zoom: 12)
+            let camera = GMSCameraPosition.camera(withLatitude: initialCoordinates.latitude, longitude: initialCoordinates.longitude, zoom: 13)
             mapView = GMSMapView.map(withFrame: view.frame, camera: camera)
             view.addSubview(mapView)
         }
@@ -27,17 +35,19 @@ final class MapViewController: UIViewController {
     
     private func setupUI() {
 //        drawRoute(routeName: BusRoutes.list.first ?? "")
-        drawRoute(routeName: "10")
-//        drawAllRoutes()
+//        drawRoute(routeName: "2")
+        drawAllRoutes()
 //        drawAllBusStops()
+//        drawAllLiveRoutes()
     }
+    
+    // MARK: Map Drawing
     
     private func drawRoute(routeName: String) {
         if let url = Bundle.main.url(forResource: routeName, withExtension: "json"),
            let data = try? Data(contentsOf: url) {
             if let gps = try? JSONDecoder().decode(GPS.self, from: data) {
                 drawRoute(from: gps)
-                drawBusStops(gps.busStops)
             }
         }
     }
@@ -54,20 +64,21 @@ final class MapViewController: UIViewController {
         }
         let polygon = GMSPolygon(path: path)
         polygon.fillColor = .clear
-        polygon.strokeColor = .black
-        polygon.strokeWidth = 2
+        polygon.strokeColor = .systemBlue
+        polygon.strokeWidth = 3
         polygon.map = mapView
         
         gps.buses?.forEach { bus in
             let marker = GMSMarker()
-            marker.icon = UIImage(named: "Arrow")
             marker.rotation = CLLocationDegrees(bus.c)
             marker.zIndex = 1000
             marker.position = CLLocationCoordinate2D(latitude: bus.lat, longitude: bus.lon)
+            marker.icon = UIImage(named: "Arrow")
             marker.title = bus.name
             marker.snippet = "Speed: \(bus.s) km/h"
             marker.map = mapView
         }
+        drawBusStops(gps.busStops)
     }
     
     private func drawAllRoutes() {
@@ -77,10 +88,14 @@ final class MapViewController: UIViewController {
     }
     
     private func drawBusStops(_ busStops: [BusStop]) {
-        busStops.forEach { busStop in
-            if let latitide = busStop.location.first, let longitude = busStop.location.last {
+        busStops.enumerated().forEach { index, busStop in
+            if let latitude = busStop.location.first, let longitude = busStop.location.last {
+                if initialCoordinates == nil, index == 0 {
+                    initialCoordinates = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+                }
                 let marker = GMSMarker()
-                marker.position = CLLocationCoordinate2D(latitude: latitide, longitude: longitude)
+                marker.position = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+//                marker.icon = UIImage(named: "MapPin")
                 marker.title = busStop.name
 //                marker.snippet = busStop.stop_id
                 marker.map = mapView
@@ -102,5 +117,82 @@ final class MapViewController: UIViewController {
         }
         drawBusStops(Array(busStops))
     }
+    
+    // MARK: - Live
+    
+    private func drawAllLiveRoutes() {
+        if let url = Bundle.main.url(forResource: "routes", withExtension: "json"),
+           let data = try? Data(contentsOf: url) {
+            if let routes = try? JSONDecoder().decode(Routes.self, from: data) {
+                routes.routes.enumerated().forEach { index, route in
+//                    guard index == 0 else { return }
+                    DispatchQueue.main.asyncAfter(deadline: .now() + Double(index) - 0.5) { [weak self] in
+                        self?.getRoute(with: route.id) { [weak self] gps, error in
+                            if let gps = gps {
+                                self?.drawRoute(from: gps)
+                            } else if let error = error {
+                                print(error)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    // MARK: Network
+    
+    func getRoute(with routeId: String, completion: @escaping (GPS?, Swift.Error?) -> Void) {
+        routeDataTask?.cancel()
+        
+        let sessionDelegate = SessionDelegate()
+        let session = URLSession(configuration: sessionConfiguration, delegate: sessionDelegate, delegateQueue: OperationQueue.main)
+        
+        guard let url = URL(string: "\(Constants.baseURL)/get-live-bus-stop-time?") else {
+            return
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        if let data = "routeId=\(routeId)".data(using: .utf8) {
+            request.httpBody = data
+            routeDataTask = session.dataTask(with: request) { [weak self] (data, response, error) in
+                guard let self = self else { return }
+                defer {
+                    self.routeDataTask = nil
+                }
+                if let error = error {
+                    completion(nil, error)
+                } else if let data = data, let response = response as? HTTPURLResponse {
+                    print(response.statusCode)
+                    if response.statusCode == 200 {
+                        let gps = try! JSONDecoder().decode(GPS.self, from: data)
+                        completion(gps, nil)
+                    } else if let error = error {
+                        completion(nil, error)
+                    }
+                }
+            }
+            routeDataTask?.resume()
+        }
+    }
 }
 
+final class SessionDelegate: NSObject, URLSessionDelegate {
+    
+    func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+        
+        completionHandler(.useCredential, nil)
+    }
+}
+
+struct Route: Codable {
+    
+    let number: String
+    let id: String
+}
+
+
+struct Routes: Codable {
+    
+    let routes: [Route]
+}
